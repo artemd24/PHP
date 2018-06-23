17a10d3fef3414e26e15f0e7e4374fcd3b2b422c
ArtemDavletov <Tomas-Cooper@mail.ru>
2018-06-23 11:39:06 +0300

    Add files via upload

--- add.php
+++ add.php
@@ -1,31 +1,14 @@
-<html>
-<body>
-
 <?php
 require('connection.php');
-$id_doc = $_GET["id"];
-if (is_null($id_doc)) {
-    exit;
-}
+$id_doc = $_GET["id_doc"];
+$id_club = $_GET["id_club"];
+$name = $_GET["name"];
+$breed = $_GET["breed"];
+$old = $_GET["old"];
+$sex = $_GET["sex"];
 $connection = mysqli_connect($host, $user, $password, $db);
-$result = mysqli_query($connection, "SELECT DISTINCT id_doc, id_club, name, breed, old, sex FROM member;");
-$member = mysqli_fetch_assoc($result);
+$sql = "INSERT INTO member (id_doc, id_club, name, breed, old, sex)
+		VALUES ('" . $id_doc . "', '" . $id_club . "', '" . $name . "', '" . $breed . "', '" . $old . "', '" . $sex . "');";
+$result = mysqli_query($connection, $sql);
+echo("<script>location.href='list.php'</script>");
 ?>
-
-<form action="dbupdate.php" method="get">
-	<input type="hidden" name="id_doc" value=<?php echo $member['id_doc']; ?>><br>
-    id_club:
-    <input type="text" name="id_club" value=<?php echo $member['id_club']; ?>><br>
-    name:
-    <input type="text" name="name" value=<?php echo $member['name']; ?>><br>
-    breed:
-    <input type="text" name="breed" value=<?php echo $member['breed']; ?>><br>
-    old:
-    <input type="text" name="old" value=<?php echo $member['old']; ?>><br>
-    sex:
-    <input type="text" name="sex" value=<?php echo $member['sex']; ?>><br>
-    <input type="submit">
-</form>
-
-</body>
-</html>
